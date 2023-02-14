4ef788bc8476b8885a3d3f30d4529f8cf19e4df3
linneszyx <itssuryanshcooldude346@gmail.com>
2023-02-14 21:56:45 +0530

    initial commit

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+const Card = () => {
+    return (
+        <div className="bg-light-purple dib br3 pa3 ma2 grow">
+            <img src="https://randomoutputs.com/assets/images/tools/faces/rgedofrx.webp" alts="people" />
+            <div>
+                <h2>CoopsWin</h2>
+                <p>coopswin@gmail.com</p>
+            </div>
+        </div>
+    );
+}
+export default Card;
