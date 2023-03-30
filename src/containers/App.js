eff5d41090c329f7675f6b99d5c48e8e8db5502b
linneszyx <itssuryanshcooldude346@gmail.com>
2023-03-30 12:24:42 +0530

    optimizing using redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,45 +1,48 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import ErrorBoundary from "../components/ErrorBoundary";
+import { setSearchField, requestPeople } from "../actions";
+
 import "./App.css";
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      people: [],
-      searchfield: "",
-    };
-  }
-  componentDidMount() {
-     fetch("https://jsonplaceholder.typicode.com/users")
-       .then((response) => response.json())
-       .then((user) => this.setState({ people: user }));
-  }
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value });
+const mapStateToProps = (state) => {
+  return {
+    searchField: state.searchPeople.searchField,
+    people: state.requestPeople.people,
+    isPending: state.requestPeople.isPending,
+    error: state.requestPeople.error,
   };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestPeople: () => dispatch(requestPeople()),
+  };
+};
+class App extends Component {
+  componentDidMount() {
+    this.props.onRequestPeople();
+  }
   render() {
-    const {people,searchfield} = this.state
+    const { searchField, onSearchChange, people, isPending } = this.props;
     const filterpeople = people.filter((people) => {
-      return people.name
-        .toLowerCase()
-        .includes(searchfield.toLowerCase());
+      return people.name.toLowerCase().includes(searchField.toLowerCase());
     });
-    return !people.length ?
-       <h1 className="tc">Loading</h1> :
-       (
-        <div className="tc">
-          <h1 className="f2">FINDI FRIENDS</h1>
-          <SearchBox searchChange={this.onSearchChange} />
-          <Scroll>
-            <ErrorBoundary>
+    return isPending ? (
+      <h1 className="tc">Loading</h1>
+    ) : (
+      <div className="tc">
+        <h1 className="f2">FINDI FRIENDS</h1>
+        <SearchBox searchChange={onSearchChange} />
+        <Scroll>
+          <ErrorBoundary>
             <CardList people={filterpeople} />
-            </ErrorBoundary>
-          </Scroll>
-        </div>
-      );
+          </ErrorBoundary>
+        </Scroll>
+      </div>
+    );
   }
 }
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
